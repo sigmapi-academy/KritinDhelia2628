251897facd5615c3b57e4f54e9659aade61f5ee2
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-06-09 07:01:08 +0530

    Started with Number System

---
+++ Coding/Kritin11/Loops/Coverersion.java
@@ -0,0 +1,22 @@
+package Loops;
+
+
+/**
+ * Write a description of class Coverersion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Coverersion
+{
+    public static void convertIntegerToBinary(int v){
+        System.out.print("\nBinary equivalent of " + v +" is: " +
+        Integer.toBinaryString(v));
+        System.out.print("\n?: "+Integer.toUnsignedString(110, 2));
+        
+        System.out.print("\n ??: "+Integer.toString(1101, 16));
+        
+        
+        Integer.to
+    }
+}
